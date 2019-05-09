284bf91f5996c0e7fb44860f84cc1e1934f46b19
Sandokaaan <30953425+Sandokaaan@users.noreply.github.com>
2019-05-09 09:41:42 +0200

    Update validation.cpp
    Implementation of the EarthCoin unicate rules for block reward calculation.

--- src/validation.cpp
+++ src/validation.cpp
@@ -1165,15 +1165,66 @@ bool ReadRawBlockFromDisk(std::vector<uint8_t>& block, const CBlockIndex* pindex
 
 CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
 {
-    int halvings = nHeight / consensusParams.nSubsidyHalvingInterval;
-    // Force block reward to zero when right shift is undefined.
-    if (halvings >= 64)
-        return 0;
+    const CAmount nMinSubsidy = 1 * COIN;
+    // base payout
+    CAmount nSubsidy = 10000 * COIN;
 
-    CAmount nSubsidy = 50 * COIN;
-    // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
-    nSubsidy >>= halvings;
-    return nSubsidy;
+    // first block receives 2% premine to support EarthCoin
+    if (nHeight == 1) {
+	    nSubsidy = 270000000LL * COIN;
+	    return nSubsidy;
+    }
+
+    // relative height inside main period (approx. 1 year)
+    int nHeightM = nHeight % 525600;
+    double phase = ((double)nHeightM) / 525600.0 * 2.0 * M_PI;
+    // modify base payout with seasonal effect
+    nSubsidy += ((int)(2000.0 * sin(phase))) * COIN;
+
+    // bonus zones
+
+    // get number of days since the inception of EarthCoin
+    int day = nHeight / 1440 + 1;
+
+    // regular bonus zones
+
+    // every 31 days, payout is increased by a factor of 5
+    if (day % 31 == 0) {
+	    nSubsidy *= 5;
+    }
+    // every 14 days, payout is increased by a factor of 2
+    else if (day % 14 == 0) {
+	    nSubsidy *= 2;
+    }
+
+    // special bonus zones
+
+    // the first three days were special (approx. 12/21-21/24 in the year of 2013)
+    switch (day) {
+        // 5 times the normal payout on day 1
+	case 1:
+	    nSubsidy *= 5;
+	    break;
+	// 3 times the normal payout on day 2
+	case 2:
+	    nSubsidy *= 3;
+	    break;
+	// 2 times the normal payout on day 3
+	case 3:
+	    nSubsidy *= 2;
+	    break;
+    }
+
+    // subsidy is cut in half every 525600 blocks,
+    // which will occur approximately every 12 months
+    nSubsidy >>= (nHeight / 525600);
+
+    // nevertheless, there will a minimum payout of 1 EarthCoin
+    if (nSubsidy < nMinSubsidy) {
+	    nSubsidy = nMinSubsidy;
+    }
+
+    return nSubsidy;    
 }
 
 bool IsInitialBlockDownload()
